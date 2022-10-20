14cf5d4777ea86a90875451ee8f02694b323caf4
mrhodes16 <69912544+mrhodes16@users.noreply.github.com>
2022-10-19 19:36:48 -0500

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bShifts = new System.Windows.Forms.Button();
+            this.bDrop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bShifts
+            // 
+            this.bShifts.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bShifts.Location = new System.Drawing.Point(548, 48);
+            this.bShifts.Name = "bShifts";
+            this.bShifts.Size = new System.Drawing.Size(165, 131);
+            this.bShifts.TabIndex = 0;
+            this.bShifts.Text = "Open Shifts";
+            this.bShifts.UseVisualStyleBackColor = true;
+            this.bShifts.Click += new System.EventHandler(this.bShifts_Click);
+            // 
+            // bDrop
+            // 
+            this.bDrop.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bDrop.Location = new System.Drawing.Point(548, 213);
+            this.bDrop.Name = "bDrop";
+            this.bDrop.Size = new System.Drawing.Size(165, 131);
+            this.bDrop.TabIndex = 1;
+            this.bDrop.Text = "Drop Shifts";
+            this.bDrop.UseVisualStyleBackColor = true;
+            this.bDrop.Click += new System.EventHandler(this.bDrop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bDrop);
+            this.Controls.Add(this.bShifts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bShifts;
+        private System.Windows.Forms.Button bDrop;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void bShifts_Click(object sender, EventArgs e)
+        {
+
+            int option;
+            Console.WriteLine("Hello Employee! \nPlease Select an a time below: \n1.Shift 12:30 \n2.Shift 2:30 \n3.Shift 4:30");
+            string opt = Console.ReadLine();
+            option = Convert.ToInt32(opt);
+            if (option == 1)
+            {
+                string times = "12:30";
+                Console.WriteLine("You have select the shift time of {0}", times);
+            }
+        }
+
+        private void bDrop_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("The shifts open for dropping are \n1. 12:30 \n2. 3:30");
+
+        }
+    }
+}
